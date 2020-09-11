a192c1fd80837c42e3ef36a2516aa6144d6305f5
Akash <abusalehakash@gmail.com>
2020-09-11 19:21:59 +0600

    🔥 product filtering working

--- client/src/pages/CategoryProducts.js
+++ client/src/pages/CategoryProducts.js
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { Typography, Container, Grow } from '@material-ui/core';
+import { useParams, useHistory } from 'react-router-dom';
+import {
+  Typography,
+  Container,
+  Grow,
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  TextField,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { SentimentDissatisfiedOutlined } from '@material-ui/icons';
+import {
+  SentimentDissatisfiedOutlined,
+  SearchOutlined,
+} from '@material-ui/icons';
 
 import Header from '../components/Header/Header';
 import ProductsListContainer from '../components/Products/ProductsListContainer';
@@ -19,6 +32,32 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 20,
+  },
+  filterArea: {
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+    marginTop: 20,
+    padding: 15,
+    background: theme.palette.secondary.main,
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+    },
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  inputField: {
+    margin: theme.spacing(2),
+    width: '80%',
+  },
+  searchBtn: {
+    [theme.breakpoints.down('xs')]: {
+      width: 100,
+    },
+    width: 200,
   },
 }));
 
@@ -32,21 +71,61 @@ const CategoryProducts = () => {
 
   // getting the category name from the url
   const { cname } = useParams();
+  const history = useHistory();
 
+  const [category, setCategory] = useState('');
+  const [searchText, setSearchText] = useState('');
+
+  // category change handler
+  const categoryChangeHandler = (event) => {
+    history.push(`/category/${event.target.value}`);
+  };
+
+  // setting category
+  useEffect(() => setCategory(cname), [cname]);
+
+  // search handler
+  const searchHandler = async () => {
+    let cname = 'all';
+    if (category !== '') {
+      cname = category;
+    }
+
+    let responseData;
+    if (searchText !== '') {
+      try {
+        responseData = await fetch(
+          `http://localhost:8000/api/product/search/${cname}/q=${searchText}`
+        );
+      } catch (error) {
+        throw Error(error.message);
+      }
+      responseData = await responseData.json();
+      setLoadedProducts(responseData);
+    }
+  };
+
+  // fetching data
   useEffect(() => {
     try {
+      let responseData;
       const sendReq = async () => {
-        const responseData = await sendRequest(
-          `http://localhost:8000/api/product/category/${cname}`
-        );
+        if (category !== '') {
+          responseData = await sendRequest(
+            `http://localhost:8000/api/product/category/${category}`
+          );
+        } else {
+          responseData = await sendRequest(
+            'http://localhost:8000/api/product/'
+          );
+        }
 
         setLoadedProducts(responseData);
+        setSearchText('');
       };
       sendReq();
-    } catch (error) {
-      console.log(error);
-    }
-  }, [sendRequest, cname]);
+    } catch (error) {}
+  }, [sendRequest, category, cname]);
 
   // Pagination helper
   const indexOfLastPost = currentPage * productsPerPage;
@@ -60,6 +139,51 @@ const CategoryProducts = () => {
     <>
       <Header />
       <main>
+        <div className={classes.filterArea}>
+          <Typography variant="h4" color="textPrimary">
+            Filter
+          </Typography>
+          <FormControl variant="filled" className={classes.inputField}>
+            <InputLabel id="category">Category</InputLabel>
+            <Select
+              required
+              labelId="category"
+              id="category"
+              value={category}
+              onChange={categoryChangeHandler}
+            >
+              {/* <MenuItem value={'All Products'}>All Products</MenuItem> */}
+              <MenuItem value={'Fruits and Vegetables'}>
+                Fruits and Vegetables
+              </MenuItem>
+              <MenuItem value={'Meat and Fish'}>Meat and Fish</MenuItem>
+              <MenuItem value={'Snacks'}>Snacks</MenuItem>
+              <MenuItem value={'Dairy'}>Dairy</MenuItem>
+              <MenuItem value={'Cooking'}>Cooking</MenuItem>
+              <MenuItem value={'Breakfast'}>Breakfast</MenuItem>
+              <MenuItem value={'Beverage'}>Beverage</MenuItem>
+            </Select>
+          </FormControl>
+
+          <TextField
+            id="searchBox"
+            label="Search Box"
+            variant="filled"
+            className={classes.inputField}
+            value={searchText}
+            onChange={(event) => setSearchText(event.target.value)}
+          />
+          <Button
+            startIcon={<SearchOutlined />}
+            variant="contained"
+            color="primary"
+            size="large"
+            className={classes.searchBtn}
+            onClick={searchHandler}
+          >
+            Search
+          </Button>
+        </div>
         {loadedProducts.length <= 0 && (
           <Grow in timeout={500}>
             <Container className={classes.emptyContainer}>
